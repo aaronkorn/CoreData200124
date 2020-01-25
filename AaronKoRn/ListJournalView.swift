45ee92f6e552d7093f5a9b0c028febf3df06a25b
aaronkorn <aaronkorn@gmail.com>
2020-01-24 20:25:57 -0800

    Added ListJournalView

---
+++ AaronKoRn/ListJournalView.swift
@@ -0,0 +1,21 @@
+//
+//  ListJournalView.swift
+//  CoreData200124
+//
+//  Created by AARON KORN on 1/24/20.
+//  Copyright © 2020 Aaron KoRn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListJournalView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ListJournalView_Previews: PreviewProvider {
+  static var previews: some View {
+    ListJournalView()
+  }
+}
